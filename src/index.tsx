6d8cd16f40e31aab5938d0cd8d5c346960bb2ea9
Harshit Kumar <harshitkumar9030@gmail.com>
2022-08-23 10:19:00 +0000

    edits

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import {store} from "./redux/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
